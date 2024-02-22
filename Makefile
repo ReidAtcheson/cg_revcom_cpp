523adcb6e44aa94f2d07a6d4ca6387c1f3f24189
Reid Atcheson <reid.atcheson@gmail.com>
2024-02-22 15:17:06 -0500

    adding random sparse matrix generator

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 
 #Note: Might need to point LD_LIBRARY_PATH to the runtimes after building
 CXX=/home/reidatcheson/Applications/clang+llvm-17.0.6-x86_64-linux-gnu-ubuntu-22.04/bin/clang++
-CXXFLAGS=--std=c++23 --stdlib=libc++
+CXXFLAGS=--std=c++23 --stdlib=libc++ -Wall -pedantic
 
 
 
--- sparse.h
+++ sparse.h
@@ -25,12 +25,32 @@ class sparse_t{
   public:
     sparse_t() = default;
     /*Randomly generate a sparse symmetric positive definite matrix.*/
-    sparse_t(I nrows,I ncols,I spread,I nnz_per_row,F eps,std::mt19937& rng){
+    sparse_t(I nrows,I spread,I nnz_per_row,F eps,std::mt19937& rng){
+      /*Hold initially in an ordered map just to make things easy.*/
       std::map<std::pair<I,I>,F> A;
+
+      /*The distributions that will pull the column indices and nonzero values.*/
       double mean=0.0;
       double std=spread;
       std::normal_distribution<> d(mean,std);
+      std::uniform_real_distribution<> v(-1.0,1.0);
+
+      /*user inputs nnz_per_row but since we are symmetrizing this it will roughly
+       * double this value so I just half it here.*/
+      nnz_per_row/=2;
+
+
       for(I r = 0; r < nrows; r++){
+        for(I nz = 0; nz < nnz_per_row; nz++){
+          /*Make sure we don't get indices below 0 or above nrows.*/
+          I c = std::max(0,std::min(nrows - 1,d(rng) + r));
+          F val = v(rng);
+          A[{r,c}] = val;
+          A[{c,r}] = val;
+        }
+        /*Zero out the diagonal as I will modify this
+         * later to ensure positive definiteness.*/
+        A[{r,r}] = 0.0;
       }
     }
 };
